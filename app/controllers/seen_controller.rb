1098f6b78968e73f2d789ab9d9b53cd01197507d
Otavio Arruda Santos <otavioarrudasantos@gmail.com>
2021-06-04 17:48:13 -0300

    corrigindo consulta de verificacao de comunicados apos exclusao.(utilizando parametro correto)

--- app/controllers/seen_controller.rb
+++ app/controllers/seen_controller.rb
@@ -2,26 +2,15 @@ class SeenController < ApplicationController
     before_action :set_seen, only: %i[ show edit update destroy ]
     before_action :set_pet, only: [ :new, :show, :edit, :update, :destroy, :create ]
     before_action :authenticate_user!, except: [:new, :create]
-    # before_action :correct_user, only: [:destroy]
   
-    # GET /pets/1/seen 
     def index
       @pets = Pet.where(user_id: current_user.id, id: params[:pet_id])
     end
   
-    # GET /pets/1 or /pets/1.json
-    def show
-    end
-  
-    # GET /pets/new
     def new
         @seen = Seen.new
     end
-  
-    # GET /pets/1/edit
-    def edit
-    end
-  
+    
     # POST /pets or /pets.json
     def create
       # @pet = Pet.new(pet_params)
@@ -39,24 +28,11 @@ class SeenController < ApplicationController
         end
       end
     end
-  
-    # PATCH/PUT /pets/1 or /pets/1.json
-    def update
-      respond_to do |format|
-        if @pet.update(pet_params)
-          format.html { redirect_to @pet, notice: "Pet was successfully updated." }
-          format.json { render :show, status: :ok, location: @pet }
-        else
-          format.html { render :edit, status: :unprocessable_entity }
-          format.json { render json: @pet.errors, status: :unprocessable_entity }
-        end
-      end
-    end
-  
-    # DELETE /pets/1 or /pets/1.json
+ 
     def destroy
       @seen.destroy
-      @all_seens = Seen.where(pet_id: :pet_id )
+      @all_seens = Seen.where(pet_id: params[:pet_id] )
+
       if @all_seens.blank?
         @pet.status = 0
         @pet.save
@@ -66,12 +42,7 @@ class SeenController < ApplicationController
         format.json { head :no_content }
       end
     end
-  
-    def correct_user
-      @pet = current_user.pets.find_by(id: params[:id])
-      redirect_to root_path, notice:"Não Permitido" if @pet.nil?
-    end
-  
+
     private
       # Use callbacks to share common setup or constraints between actions.
       def set_pet
@@ -82,11 +53,6 @@ class SeenController < ApplicationController
         @seen = Seen.find(params[:id])
       end
   
-      # Only allow a list of trusted parameters through.
-    #   def pet_params
-    #     params.require(:seen).permit(:nome, :telefone, :pet_id)
-    #   end
-
       def seen_params
         params.require(:seen).permit(:nome, :telefone, :pet_id)
       end
