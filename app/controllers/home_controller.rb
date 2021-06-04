baefc6dcf1c6a10422d749d1b642047fa3b1e0de
Otavio Arruda Santos <otavioarrudasantos@gmail.com>
2021-06-04 15:19:47 -0300

    Ajustado consulta para considerar o parametro de paginacao, css para melhorar a apresentacao dos botoes de paginacao, html para exibir o controle de paginacao

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @pets = Pet.where.not(status: 2)
+    @pets = Pet.where.not(status: 2).paginate(page: params[:page], per_page:10)
   end
 end
